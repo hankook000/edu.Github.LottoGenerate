c3eee002d0691fdce556841ed3a0673ea366fe50
choi <choi@ok.co.kr>
2018-06-16 01:18:03 +0000

    issue 1 fix

--- src/project/Main.java
+++ src/project/Main.java
@@ -13,7 +13,7 @@ public class Main {
         
         for(int i = 0; i < 6; i++)
         {
-            result[i] = random.nextInt(45);
+            result[i] = random.nextInt(45) + 1;
         }
         
         return result;
